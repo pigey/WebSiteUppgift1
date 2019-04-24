2f163c0ea50892056e5186384297973b15d1a1ce
WilhelmGY <wilhemd.gy@gmail.com>
2019-04-24 16:16:50 +0200

    Update Musixmatchapi.js

--- Design app/www/js/Musixmatchapi.js
+++ Design app/www/js/Musixmatchapi.js
@@ -1,13 +1,14 @@
-setInterval(function() {
-//function getLyrics(){
+//var myFunction = function() {
+function getLyrics(){
 
   // ändra baby till datan från spotify api
   var trackSearch = "baby";
+  console.log(trackSearch);
   var tidigareTrack = "";
 
   if (trackSearch != tidigareTrack) {
 
-    document.getElementById("lyrics").textContent = "";
+    document.getElementById("lyricsText").textContent = "";
     $.ajax({
       type: "GET",
       data: {
@@ -21,19 +22,49 @@ setInterval(function() {
       jsonpCallback: 'jsonp_callback',
       contentType: 'application/json',
       success: function(data) {
-        var p = document.getElementById("lyricsText");
-        p.textContent = data;
-
+        console.log(p);
+        var p = trackId;
+        //document.getElementById("lyricsText");
+        //p.textContent = data;
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(jqXHR);
         console.log(textStatus);
         console.log(errorThrown);
       }
+      });
+      console.log(p);
+
+      $.ajax({
+        type: "GET",
+        data: {
+          apikey:"e087c8d65e867ab873af1f18faed98a4",
+          track_id: p,
+          format:"jsonp",
+          callback:"jsonp_callback"
+        },
+        url: "https://api.musixmatch.com/ws/1.1/track.lyrics.get",
+        dataType: "jsonp",
+        jsonpCallback: 'jsonp_callback',
+        contentType: 'application/json',
+        success: function(data) {
+          console.log(lyricsBody);
+          var b = document.getElementById("lyricsText");
+          b.textContent = lyricsBody;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR);
+          console.log(textStatus);
+          console.log(errorThrown);
+        }
     });
     tidigareTrack = trackSearch;
   }
-}, 1000);
+};
+
+getLyrics();
+
+//var timer = setInterval(myFunction, 1000);
 
 
 //function getLyricsNow(){
